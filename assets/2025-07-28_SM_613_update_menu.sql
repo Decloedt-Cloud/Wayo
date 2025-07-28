5152d4f6cf9a4c1612b3e0b91f0a1958bf0a3c85
fettah1949 <fattahallam1949@gmail.com>
2025-07-28 12:16:29 +0100

    SM-613  #comment update menu

---
+++ assets/2025-07-28_SM_613_update_menu.sql
@@ -0,0 +1,7 @@
+UPDATE formation_preprod.menus SET formation_preprod.menus.displayed_name = 'Live Classes' WHERE formation_preprod.menus.id = 9;
+UPDATE formation_preprod.menus SET formation_preprod.menus.sort_order = '21' WHERE formation_preprod.menus.id = 125;
+UPDATE formation_preprod.menus SET formation_preprod.menus.sort_order = '22' WHERE formation_preprod.menus.id = 19;
+UPDATE formation_preprod.menus SET formation_preprod.menus.sort_order = '26' WHERE formation_preprod.menus.id = 136;
+UPDATE formation_preprod.menus SET formation_preprod.menus.sort_order = '27' WHERE formation_preprod.menus.id = 129;
+UPDATE formation_preprod.menus SET formation_preprod.menus.sort_order = '23' WHERE formation_preprod.menus.id = 29;
+UPDATE formation_preprod.menus SET formation_preprod.menus.parent = '0' WHERE formation_preprod.menus.id = 29;
