fb39520f5d678a6b39047f21bd72ff85c3d4fa78
fettah1949 <fattahallam1949@gmail.com>
2025-09-18 10:32:12 +0100

    remove subject

--- application/views/backend/admin/syllabus/create.php
+++ application/views/backend/admin/syllabus/create.php
@@ -25,12 +25,6 @@
 
 
         <div class="form-group col-md-12 mb-2">
-            <label for="subject_id_on_create"><?php echo get_phrase('subject'); ?><span class="required"> * </span></label>
-            <select class="form-control"  id="subject_id_on_create" name="subject_id" requied>
-                <option><?php echo get_phrase('select_a_subject'); ?></option>
-            </select>
-        </div>
-        <div class="form-group col-md-12 mb-2">
             <label for="syllabus_file"><?php echo get_phrase('upload_syllabus'); ?><span class="required"> * </span></label>
             <div class="custom-file-upload d-inline-block">
                 <input type="file" class="form-control" id="syllabus_file" name = "syllabus_file" required>
