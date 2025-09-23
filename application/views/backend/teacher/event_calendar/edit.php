4ac3aa4f81840b6cf86f010d4908c24491fcccf3
fettah1949 <fattahallam1949@gmail.com>
2025-09-23 15:20:10 +0100

    fix bug event

--- application/views/backend/teacher/event_calendar/edit.php
+++ application/views/backend/teacher/event_calendar/edit.php
@@ -12,13 +12,13 @@
             </div>
             <div class="form-group mb-1">
                 <label for="starting_date"><?php echo get_phrase('event_starting_date'); ?></label>
-                <input type="text" value="<?php echo date('m/d/Y', strtotime($event_calendar['starting_date'])); ?>" class="form-control" id="starting_date" name = "starting_date" data-provide = "datepicker" required>
+                <input type="date" value="<?php echo date('Y-m-d', strtotime($event_calendar['starting_date'])); ?>" class="form-control" id="starting_date" name = "starting_date" data-provide = "datepicker" required>
                 <small id="name_help" class="form-text text-muted"><?php echo get_phrase('provide_starting_date'); ?></small>
             </div>
 
             <div class="form-group mb-1">
                 <label for="ending_date"><?php echo get_phrase('event_ending_date'); ?></label>
-                <input type="text" value="<?php echo date('m/d/Y', strtotime($event_calendar['ending_date'])); ?>" class="form-control" id="ending_date" name = "ending_date" data-provide = "datepicker" required>
+                <input type="date" value="<?php echo date('Y-m-d', strtotime($event_calendar['ending_date'])); ?>" class="form-control" id="ending_date" name = "ending_date" data-provide = "datepicker" required>
                 <small id="name_help" class="form-text text-muted"><?php echo get_phrase('provide_ending_date'); ?></small>
             </div>
 
