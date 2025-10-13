1a7b7064f6be32f4f092caa963680973c03a22b9
fettah1949 <fattahallam1949@gmail.com>
2025-10-13 17:26:12 +0100

    fix version 3 lang

--- application/views/backend/admin/class/edit.php
+++ application/views/backend/admin/class/edit.php
@@ -66,7 +66,7 @@
 
             <!-- Message d'avertissement -->
             <small id="price-warning" class="text-danger" <?php if ($type != 'Particulier'): ?>   style="display:none;" <?php endif; ?>>
-                <?php echo get_phrase('As_you_are_a_private_individual_the_price_will_be_automatically_set_to_0'); ?>
+                <?php echo get_phrase('as_you_are_a_private_individual_the_price_will_be_automatically_set_to_0'); ?>
             </small>
         </div>
 
