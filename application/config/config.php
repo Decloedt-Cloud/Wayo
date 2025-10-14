b8236aa5bb9781054226de481c4e5ac0a212f0ce
fettah1949 <fattahallam1949@gmail.com>
2025-10-14 15:54:05 +0100

    add preprodwayosocial domaine name

--- application/config/config.php
+++ application/config/config.php
@@ -649,4 +649,5 @@ $config['enable_hooks'] = TRUE;
 
 
 // Dans config.php ou constants.php
-$config['humhub_url'] = 'https://humhub.wayo.site/';
+$config['humhub_url'] = 'https://preprodwayosocial.wayo.site/';
+    
