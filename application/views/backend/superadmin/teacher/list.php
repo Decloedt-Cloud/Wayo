145cc19f6cd58d4a1c09fedfcc21c2526eba391d
HamzaBastorDecloedt <hamza.bastor@decloedt.cloud>
2025-10-23 09:08:33 +0100

    SM-740 add profil mentor

--- application/views/backend/superadmin/teacher/list.php
+++ application/views/backend/superadmin/teacher/list.php
@@ -41,6 +41,7 @@ if ($check_data->num_rows() > 0): ?>
                         <div class="dropdown text-center">
                             <button type="button" class="btn btn-sm btn-icon btn-rounded btn-outline-secondary dropdown-btn1 dropdown-btn dropdown-toggle arrow-none card-drop" data-bs-toggle="dropdown" aria-expanded="false"><i class="mdi mdi-dots-vertical"></i></button>
                             <div class="dropdown-menu dropdown-menu-end">
+                                <a href="javascript:void(0);" class="dropdown-item" onclick="largeModal('<?php echo site_url('modal/popup/teacher/profile/'.$teacher['id'])?>', '<?php echo $this->db->get_where('schools', array('id' => $school_id))->row('name'); ?>')"><?php echo get_phrase('profile'); ?></a>
                                 <!-- item-->
                                 <a href="javascript:void(0);" class="dropdown-item" onclick="rightModal('<?php echo site_url('modal/popup/teacher/permission_overview/' . $teacher['id'] . '/' . $teacher['user_id']); ?>', '<?php echo get_phrase('assigned_permissions'); ?>')"><?php echo get_phrase('permissions'); ?></a>
                                 <!-- item-->
