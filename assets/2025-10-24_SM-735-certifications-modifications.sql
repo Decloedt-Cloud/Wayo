892f6ba0f30b9385c8fefc0310d41b85987ab6cf
fettah1949 <fattahallam1949@gmail.com>
2025-10-28 15:20:42 +0100

    fix conf

--- assets/2025-10-24_SM-735-certifications-modifications.sql
+++ assets/2025-10-24_SM-735-certifications-modifications.sql
@@ -1,7 +1,9 @@
-UPDATE menus
-SET status = 0
-WHERE displayed_name = 'exam';
+
 
 UPDATE menus
 SET displayed_name = 'certifications'
+WHERE displayed_name = 'exam';
+
+UPDATE menus
+SET status = 0
 WHERE displayed_name = 'exam';
