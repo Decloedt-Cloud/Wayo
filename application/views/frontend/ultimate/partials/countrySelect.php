47e9fc017012be66522378f157d858a9fa396a7b
mouhssineDecloedt <mouhssine.tchoubi@decloedt.cloud>
2025-11-07 17:08:29 +0100

    SM-678 front page: contact

---
+++ application/views/frontend/ultimate/partials/countrySelect.php
@@ -0,0 +1,21 @@
+<select id="countrySelect" class="form-select shadow-none">
+    <option value="+212">🇲🇦 +212</option>
+    <option value="+971" selected>🇦🇪 +971</option>
+    <option value="+33">🇫🇷 +33</option>
+    <option value="+1">🇺🇸 +1</option>
+    <option value="+44">🇬🇧 +44</option>
+    <option value="+213">🇩🇿 +213</option>
+    <option value="+216">🇹🇳 +216</option>
+    <option value="+966">🇸🇦 +966</option>
+    <option value="+20">🇪🇬 +20</option>
+    <option value="+962">🇯🇴 +962</option>
+    <option value="+961">🇱🇧 +961</option>
+    <option value="+90">🇹🇷 +90</option>
+    <option value="+49">🇩🇪 +49</option>
+    <option value="+34">🇪🇸 +34</option>
+    <option value="+39">🇮🇹 +39</option>
+    <option value="+32">🇧🇪 +32</option>
+    <option value="+41">🇨🇭 +41</option>
+    <option value="+31">🇳🇱 +31</option>
+    <option value="+351">🇵🇹 +351</option>
+</select>
