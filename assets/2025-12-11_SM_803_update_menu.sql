fd985f016ae80c3ccd4fef0e43a3d080abba14b2
mouhssineDecloedt <mouhssine.tchoubi@decloedt.cloud>
2025-12-22 16:22:07 +0100

    update requete sql

--- assets/2025-12-11_SM_803_update_menu.sql
+++ assets/2025-12-11_SM_803_update_menu.sql
@@ -1,9 +1,1 @@
-INSERT INTO menus (
-    id, displayed_name, route_name, parent, icon, status,
-    superadmin_access, admin_access, teacher_access, student_access,
-    accountant_access, librarian_access, sort_order, is_addon, unique_identifier
-) VALUES (
-    144, 'invoice', 'invoice', 0, 'fas fa-file-invoice', 1,
-    0, 0, 0, 1,
-    0, 0, 13, 0, 'management'
-);
+UPDATE formation_preprod.menus SET student_access = '1' WHERE (id = '33');
